2a7eed18e24b7ecb1f8004c8b42fa0498205c9ed
Robin Röper <rr@linux.com>
2019-11-02 03:16:50 +0100

    Add Basic Packages

--- scripts/githooks.js
+++ scripts/githooks.js
@@ -2,9 +2,9 @@ const {exec} = require('child_process');
 
 exec('git config core.hooksPath .githooks', (error, stdout, stderr) => {
     if (error) {
-        throw `[BOOTSTRAP-ENV] ${error}`;
+        throw `⚠️ [BOOTSTRAP-ENV] ${error}`;
     }
 
-    console.log('[BOOTSTRAP-ENV] Success!')
+    console.log('🚜 [BOOTSTRAP-ENV] Success')
 });
 
